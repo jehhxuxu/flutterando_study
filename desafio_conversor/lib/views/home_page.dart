1a394295e00e6091df4a5e9531cdfd74bf812d5c
jehpereira2017 <je.mary0804@gmail.com>
2021-01-18 23:17:21 -0300

    atualizando conversor

---
+++ desafio_conversor/lib/views/home_page.dart
@@ -0,0 +1,136 @@
+import 'package:desafio_conversor/models/currency_model.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        child: _body(),
+      ),
+    );
+  }
+
+  Widget _body() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          ClipRRect(
+            borderRadius: BorderRadius.circular(50),
+            child: Image.asset(
+              'assets/images/conversor.png',
+              height: 130,
+              width: 130,
+            ),
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                flex: 1,
+                child: Container(
+                  height: 56,
+                  child: DropdownButton<CurrencyModel>(
+                    isExpanded: true,
+                    underline: Container(
+                      height: 1,
+                      color: Color(0xffFFCB05),
+                    ),
+                    onChanged: (value) {},
+                    items: [
+                      DropdownMenuItem(
+                        child: Text('Real'),
+                      ),
+                      DropdownMenuItem(
+                        child: Text('Dolar'),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 20,
+              ),
+              Expanded(
+                flex: 2,
+                child: TextFormField(
+                  cursorColor: Color(0xffFFCB05),
+                  decoration: InputDecoration(
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color(0xffFFCB05),
+                      ),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color(0xffFFCB05),
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          RaisedButton(
+            onPressed: () {},
+            padding: EdgeInsets.all(15),
+            color: Color(0xffFFCB05),
+            child: Text(
+              'CONVERTER',
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget currencyConverter() {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          height: 55,
+          child: Expanded(
+            flex: 1,
+            child: DropdownButton<CurrencyModel>(
+              onChanged: null,
+              items: [
+                DropdownMenuItem(
+                  child: Text('Real'),
+                ),
+                DropdownMenuItem(
+                  child: Text('Dolar'),
+                ),
+              ],
+            ),
+          ),
+        ),
+        SizedBox(
+          width: 20,
+        ),
+        Expanded(flex: 2, child: TextFormField())
+      ],
+    );
+  }
+}
